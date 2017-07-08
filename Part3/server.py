24bda6a8bf6428545822094eb727f390d960a72c
gab <gab@cin.ufpe.br>
2017-07-08 16:43:24 -0300

    servidor compilou falta testar (ops, não)

--- Part3/server.py
+++ Part3/server.py
@@ -8,33 +8,37 @@ playMsg = 'PLAY'
 bufferSize = 1024
 
 
-def handlePlayRequest(IPset, qeue, addr):
-        (ip, port) = addr
-        if ip not in IPset:
-                queue.append(addr) 
+def handlePlayRequest(addrset, qeue, addr):
+        if addr not in addrset:
+                queue.append(addr)
+                addrset.add(addr)
 
 def getRequest(sock):
         data, addr = sock.recvfrom(bufferSize)
-        while str(data) != playMsg:
+        msg = pickle.loads(data)
+        while msg != playMsg:
                 data, addr = sock.recvfrom(bufferSize)
+                msg = pickle.loads(data)
         return addr
         
-def matchPlayers(sock, queue):
+def matchPlayers(sock, addrset, queue):
         while len(queue) >= 2:
                 addr1 = queue.popleft()
                 addr2 = queue.popleft()
                 sock.sendto(pickle.dumps(addr2), addr1)
-                sock.sendto(pickle.dumps(addr1), addr2) 
+                sock.sendto(pickle.dumps(addr1), addr2)
+                addrset.remove(addr1)
+                addrset.remove(addr2)
 
 def main():
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         serverSocket.bind((serverHost, serverPort))
-        receivedIPs = set()
+        receivedAddr = set()
         playersQueue = deque()
         while True:
                 addr = getRequest(serverSocket)
-                handlePlayRequest(receivedIPs, playersQueue, addr)
-                matchPlayers(serverSocket, playersQueue)
+                handlePlayRequest(receivedAddr, playersQueue, addr)
+                matchPlayers(serverSocket, receivedAddr, playersQueue)
 
 if __name__ == '__main__':
         main()
